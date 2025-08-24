8a31572235ed0222e015e310d3cc563bccdc3996
samuel dorven <sdorven@outlook.fr>
2025-08-24 20:55:29 +0200

    Ajout de liste personnalisé

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,43 @@
 class ListsController < ApplicationController
-  
+
+  before_action :set_list, only: [:show, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @lists = current_user.lists
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    if @list.save
+      redirect_to list_path(@list), notice: "Liste créée."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  def show
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name,)
+  end
+
 end
