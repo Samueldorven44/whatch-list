b6c99d6a41122eead8f15bbaa077ffa72a5f5a3a
samuel dorven <sdorven@outlook.fr>
2025-09-13 14:01:36 +0200

    carrousel home page mobile

---
+++ app/javascript/controllers/splide_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import Splide from "@splidejs/splide"
+
+export default class extends Controller {
+  connect() {
+    var splide = new Splide('.splide', {
+      type: 'loop',
+      padding: '5rem',
+      gap: '1.5rem'
+    });
+
+    splide.mount();
+  }
+}
